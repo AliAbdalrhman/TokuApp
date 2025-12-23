f888de5c32bdf1b12acc837d8b4eff3a2c1575a1
Ali Abdelrahman <ali.abdalrhman.606@gmail.com>
2025-12-23 20:12:50 +0200

    Refactor app and make separate home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toku_app/screens/home_page.dart';
 
 void main() {
   runApp(TokuApp());
@@ -9,66 +10,6 @@ class TokuApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Color(0xffFEF6DB),
-        appBar: AppBar(
-          title: Text('Toku', style: TextStyle(color: Colors.white)),
-          backgroundColor: Color(0xFF46322B),
-        ),
-        body: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.only(left: 24),
-              alignment: Alignment.centerLeft,
-              width: double.infinity,
-              height: 65,
-              color: Color(0xffEF9235),
-              child: Text(
-                'Numbers',
-                style: TextStyle(color: Colors.white, fontSize: 18),
-              ),
-            ),
-
-            Container(
-              padding: EdgeInsets.only(left: 24),
-              alignment: Alignment.centerLeft,
-              width: double.infinity,
-              height: 65,
-              color: Color(0xff558B37),
-              child: Text(
-                'Family Members',
-                style: TextStyle(color: Colors.white, fontSize: 18),
-              ),
-            ),
-
-            Container(
-              padding: EdgeInsets.only(left: 24),
-              alignment: Alignment.centerLeft,
-              width: double.infinity,
-              height: 65,
-              color: Color(0xff79459F),
-              child: Text(
-                'Colors',
-                style: TextStyle(color: Colors.white, fontSize: 18),
-              ),
-            ),
-
-            Container(
-              padding: EdgeInsets.only(left: 24),
-              alignment: Alignment.centerLeft,
-              width: double.infinity,
-              height: 65,
-              color: Color(0xff50ADC7),
-              child: Text(
-                'Phrases',
-                style: TextStyle(color: Colors.white, fontSize: 18),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage());
   }
 }
