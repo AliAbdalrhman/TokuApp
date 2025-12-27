268b584e4049f41effc64e120eaabd8487dc6d06
Ali Abdelrahman <ali.abdalrhman.606@gmail.com>
2025-12-27 09:00:19 +0200

    Add Number page with its navigation function

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
 
 class Category extends StatelessWidget {
+  Category({this.text, this.color, this.onTap});
+
   String? text;
   Color? color;
-
-  Category({this.text, this.color});
+  Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(left: 24),
-      alignment: Alignment.centerLeft,
-      width: double.infinity,
-      height: 65,
-      color: color,
-      child: Text(text!, style: TextStyle(color: Colors.white, fontSize: 18)),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.only(left: 24),
+        alignment: Alignment.centerLeft,
+        width: double.infinity,
+        height: 65,
+        color: color,
+        child: Text(text!, style: TextStyle(color: Colors.white, fontSize: 18)),
+      ),
     );
   }
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toku_app/components/category_item.dart';
+import 'package:toku_app/screens/numbers_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -14,10 +15,27 @@ class HomePage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Category(text: 'Numbers', color: Color(0xffEF9235)),
-          Category(text: 'Family Members', color: Color(0xff558B37)),
-          Category(text: 'Colors', color: Color(0xff79459F)),
-          Category(text: 'Phrases', color: Color(0xff50ADC7)),
+          Category(
+            text: 'Numbers',
+            color: Color(0xffEF9235),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (BuildContext context) {
+                    return NumberPage();
+                  },
+                ),
+              );
+            },
+          ),
+          Category(
+            text: 'Family Members',
+            color: Color(0xff558B37),
+            onTap: () {},
+          ),
+          Category(text: 'Colors', color: Color(0xff79459F), onTap: () {}),
+          Category(text: 'Phrases', color: Color(0xff50ADC7), onTap: () {}),
         ],
       ),
     );
